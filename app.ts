30ad18d6ae70ef8dc71da78d456a10932ac0f5db
omar ahmed <50985259+omarazam163@users.noreply.github.com>
2025-03-04 17:40:35 +0200

    deployment

--- app.ts
+++ app.ts
@@ -1,19 +1,20 @@
 import express from "express";
 import { userRouter } from "./routers/user.router";
 import { leaderBoaerdRouter } from "./routers/leaderBoard.router";
-import { Request, Response, NextFunction, ErrorRequestHandler } from "express";
 import { GlobalErrorHandler } from "./helpers/globalErrorHandler";
-let app = express();
 
-app.use(express.json());
-app.use("/user", userRouter);
-app.use("/leaderboard", leaderBoaerdRouter);
+
+export const App = express();
+
+App.use(express.json());
+App.use("/user", userRouter);
+App.use("/leaderboard", leaderBoaerdRouter);
 let port: number = 3000;
 
-app.use(
+App.use(
     GlobalErrorHandler
 );
 
-app.listen(port, () => {
+App.listen(port, () => {
   console.log(`server started at http://localhost:${port}`);
 });
