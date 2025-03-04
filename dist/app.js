40ea7ae4386866ad25e6e6f48d58c9b6c7d177ba
omar ahmed <50985259+omarazam163@users.noreply.github.com>
2025-03-04 18:06:35 +0200

    try deploy

--- dist/app.js
+++ dist/app.js
@@ -3,16 +3,17 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.App = void 0;
 const express_1 = __importDefault(require("express"));
 const user_router_1 = require("./routers/user.router");
 const leaderBoard_router_1 = require("./routers/leaderBoard.router");
 const globalErrorHandler_1 = require("./helpers/globalErrorHandler");
-let app = (0, express_1.default)();
-app.use(express_1.default.json());
-app.use("/user", user_router_1.userRouter);
-app.use("/leaderboard", leaderBoard_router_1.leaderBoaerdRouter);
+exports.App = (0, express_1.default)();
+exports.App.use(express_1.default.json());
+exports.App.use("/user", user_router_1.userRouter);
+exports.App.use("/leaderboard", leaderBoard_router_1.leaderBoaerdRouter);
 let port = 3000;
-app.use(globalErrorHandler_1.GlobalErrorHandler);
-app.listen(port, () => {
+exports.App.use(globalErrorHandler_1.GlobalErrorHandler);
+exports.App.listen(port, () => {
     console.log(`server started at http://localhost:${port}`);
 });
